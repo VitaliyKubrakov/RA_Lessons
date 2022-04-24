e57f9e716bbec0f10049169240cc92e3e3927387
VitaliyKubrakov <104314789+VitaliyKubrakov@users.noreply.github.com>
2022-04-24 19:41:55 +0300

    Initial commit

---
+++ Lesson2/Lesson2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Leeson2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter the name");
+
+            string word = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {word}");
+        }
+    }
+}
