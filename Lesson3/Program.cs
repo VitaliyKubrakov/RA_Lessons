4a717930d56e254f05763cd76a881eb1a66b8c0b
VitaliyKubrakov <vmkubrakov@gmail.com>
2022-05-03 02:35:03 +0300

    Lesson3

---
+++ Lesson3/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Lesson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstValue, secondValue;
+            string action;
+
+            Console.WriteLine("Enter first number");
+            Int32.TryParse(Console.ReadLine(), out firstValue);
+
+            Console.WriteLine("Enter second number");
+            Int32.TryParse(Console.ReadLine(), out secondValue);
+
+            Console.WriteLine("Enter action (+ - / * %)");
+            action = Console.ReadLine();
+
+            switch (action)
+            {
+                case "+":
+                    Console.WriteLine(firstValue + secondValue);
+                    break;
+                case "-":
+                    Console.WriteLine(firstValue - secondValue);
+                    break;
+                case "/":
+                    if (secondValue == 0)
+                    {
+                        Console.WriteLine(0);
+                        break;
+                    }
+                    Console.WriteLine(firstValue / secondValue);
+                    break;
+                case "*":
+                    Console.WriteLine(firstValue * secondValue);
+                    break;
+                case "%":
+                    Console.WriteLine(firstValue % secondValue);
+                    break;
+
+                default:
+                    Console.WriteLine("Incorrect action entered");
+                    break;
+            }
+
+            
+        }
+    }
+}
