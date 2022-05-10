ca86cca410ff4702a2d375938f677b63718884ed
VitaliyKubrakov <vmkubrakov@gmail.com>
2022-05-10 15:53:37 +0300

    Lesson 5

---
+++ Lesson5/Lesson5/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Lesson5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter coordinates of the first Vector (x,y,z):");
+            Int32.TryParse(Console.ReadLine(), out var x);
+            Int32.TryParse(Console.ReadLine(), out var y);
+            Int32.TryParse(Console.ReadLine(), out var z);
+
+            var vec1 = new Vector(x, y, z);
+   
+            Console.WriteLine("Enter coordinates of the second Vector (x,y,z):");
+            Int32.TryParse(Console.ReadLine(), out x);
+            Int32.TryParse(Console.ReadLine(), out y);
+            Int32.TryParse(Console.ReadLine(), out z);
+
+            var vec2 = new Vector(x, y, z);
+
+            vec1.VectorInfo();
+            vec2.VectorInfo();
+
+            var vecSum = vec1 + vec2;
+            Console.WriteLine($"vec1 + vec2 = ({vecSum.x}, {vecSum.y}, {vecSum.z})");
+            
+            var vecSub = vec1 - vec2;
+            Console.WriteLine($"vec1 - vec2 = ({vecSub.x}, {vecSub.y}, {vecSub.z})");
+
+            var vecMult = vec1 * vec2;
+            Console.WriteLine($"vec1 * vec2 = ({vecMult.x}, {vecMult.y}, {vecMult.z})");
+
+            var scalar = Vector.Scalar(vec1, vec2);
+            Console.WriteLine($"(vec1, vec2) = {scalar}");
+
+            var corner = Vector.Corner(vec1, vec2);
+            Console.WriteLine($"Corner(vec1, vec2) = {corner}");
+        }
+
+    }
+}
---
+++ Lesson5/Lesson5/Vector.cs
@@ -0,0 +1,58 @@
+﻿using static System.Math;
+
+namespace Lesson5
+
+{
+    public class Vector
+    {
+        public int x, y, z;
+        
+        public Vector()
+        {
+            x = 0;
+            y = 0;  
+            z = 0;  
+        }
+        public Vector(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public void VectorInfo()
+        {
+            Console.WriteLine($"({x}, {y}, {z})");
+        }
+
+        public static Vector operator +(Vector vec1, Vector vec2)
+        {
+            return new Vector(vec1.x + vec2.x, vec1.y + vec2.y, vec1.z + vec2.z);
+        }
+
+        public static Vector operator -(Vector vec1, Vector vec2)
+        {
+            return new Vector(vec1.x - vec2.x, vec1.y - vec2.y, vec1.z - vec2.z);
+        }
+
+        public static Vector operator *(Vector vec1, Vector vec2)
+        {
+            return new Vector(vec1.y * vec2.z - vec1.z * vec2.y, vec1.z * vec2.x - vec1.x * vec2.z, vec1.x * vec2.y - vec1.y * vec2.x);
+        }
+
+        public static int Scalar(Vector vec1, Vector vec2)
+        {
+            return vec1.x * vec2.x + vec1.y * vec2.y + vec1.z * vec2.z;
+        }
+
+        public static double Length(Vector vec1)
+        {
+            return Math.Sqrt(vec1.x * vec1.x + vec1.y * vec1.y + vec1.z * vec1.z);
+        }
+        
+        public static double Corner(Vector vec1, Vector vec2)
+        {
+            return Scalar(vec1, vec2) / (Math.Abs(Length(vec1)) * Math.Abs(Length(vec2)));
+        }
+    }
+}
